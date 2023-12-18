1088e05d6b2975ba68e1a38bfaa1393d9f66b68e
butz-bristol <98444416+butz-bristol@users.noreply.github.com>
2023-12-18 19:17:34 +0800

    Update Packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,10 +11,19 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      alias: {
+        map: [['src', './src']],
+        extensions: ['.js', '.jsx', '.json'],
+      },
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
